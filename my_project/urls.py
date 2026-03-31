14ebc601b3d29246d28a416f33bf3c0d20972b36
picker770 <arehman897@gmail.com>
2026-03-31 23:39:34 +0100

    adjust the route url

--- my_project/urls.py
+++ my_project/urls.py
@@ -22,6 +22,7 @@ from about import views as about_views
 
 urlpatterns = [
     
+    path('', index_views.index, name='home'),
     path('hello/', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
     path("admin/", admin.site.urls),
